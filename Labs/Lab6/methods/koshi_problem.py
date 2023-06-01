1b782cfbb34dfd6903e8befb2d8e467f50386ebe
PashcalE2 <shipulinpavel@mail.ru>
2023-06-01 20:18:07 +0300

    6 con

--- Labs/Lab6/methods/koshi_problem.py
+++ Labs/Lab6/methods/koshi_problem.py
@@ -8,7 +8,7 @@ def runge_rule(y_h, y_half_h, p, epsilon):
 
 
 def euler(f=lambda x, y: 0, x_0=0, y_0=0, x_n=0, h=0, epsilon=0):
-    result = Table(head=["Номер шага", "y_h", "y_half_h", "R"])
+    result = Table(head=["Номер шага", "h", "half_h", "y_h", "y_half_h", "R"])
 
     if x_0 > x_n:
         raise Exception("x_0 > x_n???")
@@ -16,6 +16,7 @@ def euler(f=lambda x, y: 0, x_0=0, y_0=0, x_n=0, h=0, epsilon=0):
     y_h = 0
     y_half_h = math.inf
     iterations = 0
+    y_values = []
 
     while not runge_rule(y_h, y_half_h, 1, epsilon):
         iterations += 1
@@ -23,27 +24,82 @@ def euler(f=lambda x, y: 0, x_0=0, y_0=0, x_n=0, h=0, epsilon=0):
         x = x_0
         y = y_0
 
+        y_values = [y]
+
         while x < x_n:
             y = y + h * f(x, y)
             x = x + h
 
+            y_values.append(y)
+
         y_h = y_half_h
         y_half_h = y
 
-        result.add_row([iterations, y_h, y_half_h, abs(y_h - y_half_h)])
+        result.add_row([iterations, h * 2, h, y_h, y_half_h, abs(y_h - y_half_h)])
 
         h /= 2
 
-    return result
+    return result, y_values
 
 
-def runge_kutta():
-    result = Table(head=[])
+def runge_kutta(f=lambda x, y: 0, x_0=0, y_0=0, x_n=0, h=0, epsilon=0):
+    result = Table(head=["Номер шага", "h", "half_h", "y_h", "y_half_h", "R"])
 
-    return result
+    if x_0 > x_n:
+        raise Exception("x_0 > x_n???")
+
+    y_h = 0
+    y_half_h = math.inf
+    iterations = 0
+    y_values = []
+
+    while not runge_rule(y_h, y_half_h, 4, epsilon):
+        iterations += 1
+
+        x = x_0
+        y = y_0
+        y_values = [y]
+
+        while x < x_n:
+            k1 = h * f(x, y)
+            k2 = h * f(x + h / 2, y + k1 / 2)
+            k3 = h * f(x + h / 2, y + k2 / 2)
+            k4 = h * f(x + h, y + k3)
+            y = y + 1 / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
+            x = x + h
+
+            y_values.append(y)
+
+        y_h = y_half_h
+        y_half_h = y
+
+        result.add_row([iterations, h * 2, h, y_h, y_half_h, abs(y_h - y_half_h) / 15])
+
+        h /= 2
+
+    return result, y_values
 
 
-def milne():
-    result = Table(head=[])
+def milne(f=lambda x, y: 0, x_0=0, y_0=0, x_n=0, h=0, epsilon=0):
+    if x_0 > x_n:
+        raise Exception("x_0 > x_n???")
 
-    return result
+    runge_kutta_table, y_values = runge_kutta(f, x_0, y_0, x_n, h, epsilon)
+    h = runge_kutta_table.get_row(-1)[2]
+    x_values = [(x_0 + i * h) for i in range(3)]
+    y_values = y_values[:3]
+    x = x_values[-1] + h
+
+    while x < x_n:
+        y_suggestion = y_values[-3] + 4 * h / 3 * (2 * f(x_values[-3], y_values[-3]) - f(x_values[-2], y_values[-2]) + 2 * f(x_values[-1], y_values[-1]))
+        y_corrected = y_values[-2] + h / 3 * (f(x_values[-2], y_values[-2]) + 4 * f(x_values[-1], y_values[-1]) + f(x, y_suggestion))
+
+        while abs(y_suggestion - y_corrected) > epsilon:
+            y_corrected = y_values[-2] + h / 3 * (f(x_values[-2], y_values[-2]) + 4 * f(x_values[-1], y_values[-1]) + f(x, y_corrected))
+
+        x_values.append(x)
+        y_values.append(y_corrected)
+
+        x += h
+
+    return x_values, y_values
--- Labs/Lab6/runtime/Lab6Command.py
+++ Labs/Lab6/runtime/Lab6Command.py
@@ -7,7 +7,7 @@ from Labs.Lab6.data.table import Table
 from Labs.Lab6.io import converter
 from Labs.Lab6.runtime.AnyCommand import AnyCommand
 from Labs.Lab6.runtime.AnyManager import AnyManager
-from Labs.Lab6.methods.koshi_problem import euler
+from Labs.Lab6.methods.koshi_problem import euler, runge_kutta, milne
 
 
 class Lab6Command(AnyCommand):
@@ -15,30 +15,54 @@ class Lab6Command(AnyCommand):
         super().__init__(name="lab6", manager=manager, description="Лабораторная работа 6 (ДУ)")
 
         self.funcs_x_y = [
-            Equation(lambda x, y: y + (1 + x) * y ** 2, [], "y' = y + (1 + x)y ** 2"),
-            Equation(lambda x, y: x + y, [], "y' = x + y"),
-            Equation(lambda x, y: x / y, [], "y' = x / y")
+            {
+                "f(x,y)": Equation(lambda x, y: y + (1 + x) * y ** 2, [], "y' = y + (1 + x)y ** 2"),
+                "y(x)": Equation(lambda x: 0, [], "")
+            },
+            {
+                "f(x,y)": Equation(lambda x, y: x + y, [], "y' = x + y"),
+                "y(x)": Equation(lambda x: 0, [], "")
+            },
+            {
+                "f(x,y)": Equation(lambda x, y: x + y, [], "y' = x + y"),
+                "y(x)": Equation(lambda x: 0, [], "")
+            }
         ]
 
         self.funcs_table = Table(head=["Номер", "Уравнение"])
 
         for i in range(len(self.funcs_x_y)):
-            self.funcs_table.add_row([i + 1, self.funcs_x_y[i].__str__()])
+            self.funcs_table.add_row([i + 1, self.funcs_x_y[i]["f(x,y)"].__str__()])
 
     def execute(self):
         io = self.manager.get_iostream()
 
         io.output.info_msg(self.funcs_table)
         func_num = io.input.index_input(self.funcs_x_y, 1, "Введите номер уравнения") - 1
-        func = self.funcs_x_y[func_num].get_func()
+        func = self.funcs_x_y[func_num]["f(x,y)"].get_func()
+        true_y = self.funcs_x_y[func_num]["y(x)"].get_func()
 
         io.output.info_msg("Ввод y(x_0) = y_0")
         x_0 = io.input.float_input("Введите x_0")
         y_0 = io.input.float_input("Введите y_0")
         x_n = io.input.any_input(converter.str_to_float, lambda v: (v > x_0, "x_n должно быть больше чем x_0"), "Введите x_n")
-        h = io.input.any_input(converter.str_to_float, lambda v: (v < x_n - x_0, "h должно быть меньше чем x_n - x_0"), "Введите шаг h")
+        h = io.input.any_input(converter.str_to_float, lambda v: ((v < x_n - x_0), "h должно быть меньше чем x_n - x_0"), "Введите шаг h")
         epsilon = io.input.float_input("Введите точность epsilon")
 
-        io.output.info_msg(euler(func, x_0, y_0, x_n, h, epsilon))
+        euler_table, euler_y_values = euler(func, x_0, y_0, x_n, h, epsilon)
+        euler_table.float_digits = 6
+
+        runge_kutta_table, runge_kutta_y_values = runge_kutta(func, x_0, y_0, x_n, h, epsilon)
+        runge_kutta_table.float_digits = 6
+
+        milne_x_values, milne_y_values = milne(func, x_0, y_0, x_n, h, epsilon)
+        milne_table = Table(head=["Номер", "x_i", "y_i", "y_точн"], float_digits=6)
+
+        for i in range(len(milne_x_values)):
+            milne_table.add_row([i, milne_x_values[i], milne_y_values[i], true_y(milne_x_values[i])])
+
+        io.output.info_msg(f"Метод Эйлера:{euler_table}\nМетод Рунге-Кутта 4-го порядка:{runge_kutta_table}\nМетод Милна:{milne_table}")
+
+
 
         return "Лабораторная работа 6 (ДУ) завершилась"
